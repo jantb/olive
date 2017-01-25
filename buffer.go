00e28a74a2f852f1cfaec049d4618a53e740bf5d
jantb <jan@boee.org>
2017-01-25 06:54:12 +0100

    Initial commit

---
+++ buffer.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/jantb/rope"
+
+//Buffer holds the current buffer
+type Buffer struct {
+	r *rope.Rope
+}
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/jantb/rope"
+
+func main() {
+	r := rope.NewFromBytes([]byte("test"))
+
+}
