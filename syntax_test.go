4da919fe4974b5e38cead5728d80fa3415d58298
jantb <jan@boee.org>
2017-03-01 22:00:45 +0100

    new try for syntax

--- syntax_test.go
+++ syntax_test.go
@@ -36,3 +36,10 @@ func TestSyntax4(t *testing.T) {
 		fmt.Println(s)
 	}
 }
+func TestSyntax5(t *testing.T) {
+	loadDark()
+	syntax := syntax([]rune("<h1></h1>"), "main.xml")
+	for _, s := range syntax {
+		fmt.Println(s)
+	}
+}
